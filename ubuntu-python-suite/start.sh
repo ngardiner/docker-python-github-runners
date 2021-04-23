05646a9ffd83e2d7e81cf2f0bdffb0ab2925c857
Nathan Gardiner <ngardiner@gmail.com>
2021-04-23 10:09:06 +0000

    Commit additional files

---
+++ ubuntu-python-suite/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TOKEN=`curl -s -u $USER:$PAT -X POST \
+	    -H "Accept: application/vnd.github.v3+json" \
+     https://api.github.com/repos/$USER/$REPO/actions/runners/registration-token -o - | jq -r .token`
+
+echo $TOKEN
+
+# Verify the installed Python version
+#/home/docker/info.py
+
+# Install and init the Github Runner
+cd /home/docker/actions-runner
+
+./config.sh --unattended --url https://github.com/$USER/$REPO --token ${TOKEN} --name `hostname -s` --labels python${PYTHON_VERSION}
+
+cleanup() {
+	    echo "Removing runner..."
+	        ./config.sh remove --unattended --token ${TOKEN}
+	}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh --once & wait $!
+
+cleanup;
